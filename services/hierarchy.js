300bd9d7c269bfe484d3347d7f4fc63c716bfa84
dbreakers <david.breakwell@gmail.com>
2021-12-18 11:52:11 +0000

    MIssing ]

--- services/hierarchy.js
+++ services/hierarchy.js
@@ -79,7 +79,7 @@ async function getPath(id){
 }
 
 async function getChild(id){
-     const rows = await db.query(    "SELECT * FROM `hierarchy_relate` right join hierarchy_items  on hierarchy_items.org_unit_id = hierarchy_relate.org_unit_id WHERE hierarchy_relate.related_id = ? and hier_type=?", [id,"ST");
+     const rows = await db.query(    "SELECT * FROM `hierarchy_relate` right join hierarchy_items  on hierarchy_items.org_unit_id = hierarchy_relate.org_unit_id WHERE hierarchy_relate.related_id = ? and hier_type=?", [id,"ST"]);
  
   const data = helper.emptyOrRows(rows);
   
